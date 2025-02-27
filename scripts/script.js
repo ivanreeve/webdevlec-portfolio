46dc50793e01a97044923ecc0f255e9ccdd140b5
ivanreeve <ivanreeve@gmail.com>
2025-02-27 10:38:20 +0800

    fix subtitle

--- scripts/script.js
+++ scripts/script.js
@@ -2,14 +2,16 @@ function toggleMenu() {
   const darkbg = document.querySelector(".dark-bg");
   const menu = document.querySelector(".nav-icon-menu");
   const wrapper = document.querySelector(".wrapper");
-  
+
   menu.classList.toggle("active");
   darkbg.classList.toggle("active");
   wrapper.classList.toggle("menu-active");
-  
+
   // Handle body scrolling
-  document.body.style.overflow = darkbg.classList.contains("active") ? "hidden" : "auto";
-  
+  document.body.style.overflow = darkbg.classList.contains("active")
+    ? "hidden"
+    : "auto";
+
   darkbg.removeEventListener("click", removeMenu);
   if (darkbg.classList.contains("active")) {
     darkbg.addEventListener("click", removeMenu);
@@ -20,14 +22,14 @@ function removeMenu() {
   const darkbg = document.querySelector(".dark-bg");
   const menu = document.querySelector(".nav-icon-menu");
   const wrapper = document.querySelector(".wrapper");
-  
+
   menu.classList.remove("active");
   darkbg.classList.remove("active");
   wrapper.classList.remove("menu-active");
   document.body.style.overflow = "auto"; // Restore scrolling
 }
 
-document.querySelector(".fade").classList.toggle("active")
+document.querySelector(".fade").classList.toggle("active");
 
 const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
@@ -35,21 +37,21 @@ let interval = null;
 
 const target = document.querySelector(".hack-effect");
 let iteration = 0;
-  
+
 clearInterval(interval);
 
 interval = setInterval(() => {
   target.innerText = target.innerText
     .split("")
     .map((letter, index) => {
-      if(index < iteration) {
+      if (index < iteration) {
         return target.dataset.value[index];
       }
-      return letters[Math.floor(Math.random() * 26)]
+      return letters[Math.floor(Math.random() * 26)];
     })
     .join("");
-  if(iteration >= target.dataset.value.length){ 
+  if (iteration >= target.dataset.value.length) {
     clearInterval(interval);
   }
-    iteration += 1/2;
-  }, 30);
+  iteration += 1 / 2;
+}, 30);
